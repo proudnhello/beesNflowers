d71ebe3b4ca00fb9a0f9db2f50a37f32cb423131
lweislak <150990328+lweislak@users.noreply.github.com>
2025-02-28 20:57:00 -0800

    Bees move to flowers

--- js/sketch.js
+++ js/sketch.js
@@ -10,11 +10,10 @@ let bees = [];
 let hives = [];
 
 //This class stores infomation about each flower displayed on the screen
-//TODO: Don't spawn flowers directly on top of eachother or hives
+//TODO: Don't spawn flowers directly on top of hives or each other
 class Flower {
-  constructor(x, y, stigmaColor, petalColor){
-    this.x = x;
-    this.y = y; 
+  constructor(x, y, stigmaColor, petalColor) {
+    this.position = {x, y};
     this.stigmaColor = stigmaColor;
     this.petalColor = petalColor;
   }
@@ -23,13 +22,13 @@ class Flower {
   sprite() {
     noStroke();
     fill(this.petalColor);
-    ellipse(this.x,this.y,20,20);
-    ellipse(this.x-15,this.y+5,20,20);
-    ellipse(this.x-25,this.y-5,20,20);
-    ellipse(this.x-17,this.y-20,20,20);
-    ellipse(this.x,this.y-15,20,20);
+    ellipse(this.position.x,this.position.y,20,20);
+    ellipse(this.position.x-15,this.position.y+5,20,20);
+    ellipse(this.position.x-25,this.position.y-5,20,20);
+    ellipse(this.position.x-17,this.position.y-20,20,20);
+    ellipse(this.position.x,this.position.y-15,20,20);
     fill(this.stigmaColor);
-    ellipse(this.x-12,this.y-7,22,22) ;
+    ellipse(this.position.x-12,this.position.y-7,22,22) ;
   }
 }
 
@@ -52,8 +51,8 @@ class Bee extends Hive {
   constructor(color, position) {
     super(color, position); //Call parent hive class
     this.target = null;
-    this.searchRadius = 100;
-    this.position = this.beeSpawn;
+    this.searchRadius = 300;
+    this.position = {x: this.beeSpawn.x, y: this.beeSpawn.y}; //Set start position to middle of hive
   }
 
   display() {
@@ -65,20 +64,48 @@ class Bee extends Hive {
     pop();
   }
 
+  //Returns nearby flowers that bee can travel to (within radius and not the same color)
   getNeighbors() {
-
+    var neighbors = [];
+    for(let flower of flowers) {
+      var flowerDist = dist(flower.position.x, flower.position.y, this.position.x, this.position.y);
+      if(flowerDist <= this.searchRadius && flower.petalColor != this.color) {
+        neighbors.push(flower);
+      }
+    }
+    return neighbors;
   }
 
   search() {
-
+    var validNeighbors = this.getNeighbors();
+    //TODO: Assign weights based on color of flower
+    this.setTarget(validNeighbors[0]); //FIX: Assign target to flower
   }
 
+  //If bee has a target flower, move towards it. If it doesn't, search for nearby flowers
   move() {
-
+    if(this.target) {
+      this.position.x = lerp(this.position.x, this.target.position.x, 0.01);
+      this.position.y = lerp(this.position.y, this.target.position.y, 0.01);
+      if(this.targetReached()) {
+        this.target.petalColor = this.color;
+        this.target = null;
+      }
+    } else {
+      this.search();
+    }
   }
 
-  setTarget() {
+  //Helper function to check if target flower has been reached
+  targetReached() {
+    if(dist(this.position.x, this.position.y, this.target.position.x, this.target.position.y) <= 5) {
+      return true;
+    }
+    return false;
+  }
 
+  setTarget(target) {
+    this.target = target;
   }
 }
 
@@ -90,7 +117,6 @@ function resizeScreen() {
   // redrawCanvas(); // Redraw everything based on new size
 }
 
-// setup() function is called once when the program starts
 function setup() {
   // place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
@@ -132,9 +158,20 @@ function draw() {
 
   for(let bee of bees) {
     bee.display();
+    bee.move();
   }
 }
 
 function mouseClicked() {
-  flowers.push(new Flower(mouseX, mouseY, "yellow", "grey"));
+  if(canvasClicked()) {
+    flowers.push(new Flower(mouseX, mouseY, "yellow", "grey"));
+  }
+}
+
+//Helper function to determine if mouse was clicked on the canvas
+function canvasClicked() {
+	if((mouseX > 0 && mouseX < width) && (mouseY > 0 && mouseY < height)) {
+		return true;
+	}
+	return false;
 }
